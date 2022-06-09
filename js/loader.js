492ab43253e530ba45746897c0674287ac6c2866
unknown <codewithdripzy@gmail.com>
2022-06-10 00:34:18 +0100

    Added more features to stylesheet

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+// function PreLoader(){
+//     let loader = document.getElementById("loader");
+//     loader.classList.add("show-flex");
+
+//     setTimeout(()=>{
+//         loader.classList.remove("show-block");
+
+//     }, 5000)
+// }
